2f43d6393ccd5d185a79412e2829ea4f665d22e1
sryps <97310071+sryps@users.noreply.github.com>
2023-04-17 20:11:53 -0400

    godu WIP

---
+++ go.mod
@@ -0,0 +1,7 @@
+module github.com/informalsystems/godu
+
+require github.com/blacked/go-zabbix v0.0.0-20170118040903-3c6a95ec4fdc
+
+require github.com/spf13/cast v1.4.1
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	. "github.com/blacked/go-zabbix"
+	"github.com/spf13/cast"
+)
+
+var (
+	defaultHost = os.Getenv("ZABBIX_HOST_NAME")
+	defaultIP   = os.Getenv("ZABBIX_SERVER")
+	defaultPort = 10051
+	defaultDir  = os.Getenv("HOME")
+	defaultKey  = os.Getenv("ZABBIX_TRAPPER_KEY")
+)
+
+func DirSize(path string) string {
+	var dirSize int64 = 0
+
+	readSize := func(path string, file os.FileInfo, err error) error {
+		if !file.IsDir() {
+			dirSize += file.Size()
+		}
+		return nil
+	}
+
+	filepath.Walk(path, readSize)
+
+	sizeMB := float64(dirSize) / 1024 / 1024 / 1024
+	sizeGB := int(math.Floor(sizeMB*100) / 100)
+
+	size := strconv.Itoa(sizeGB)
+	return size
+}
+
+func zabbixSend(dirSize string, dir string) {
+	var metrics []*Metric
+	metrics = append(metrics, NewMetric(defaultHost, defaultKey, dir))
+	metrics = append(metrics, NewMetric(defaultHost, defaultKey, dirSize))
+
+	packet := NewPacket(metrics)
+
+	z := NewSender(defaultIP, defaultPort)
+	resp, err := z.Send(packet)
+
+	if err != nil {
+		fmt.Printf("Zabbix send failed: %v", err)
+	}
+
+	fmt.Println(cast.ToString(resp))
+}
+
+func main() {
+	var arg string
+	flag.StringVar(&arg, "f", "/path/to/your/directory", "Enter your arg")
+	flag.Parse()
+
+	run := DirSize(arg)
+	fmt.Printf("%s%s", run, " GB\n")
+	fmt.Println(arg)
+	zabbixSend(run, arg)
+}
